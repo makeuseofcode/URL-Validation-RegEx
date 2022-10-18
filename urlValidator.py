a43fbe7980022e9a85ec1ae19c5a8d6dad68911e
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2022-10-18 17:47:23 +0530

    Added url validation regex

---
+++ urlValidator.py
@@ -0,0 +1,22 @@
+import re
+
+def validateURL(url):
+    regex = "((http|https)://)(www.)?[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)"
+    r = re.compile(regex)
+
+    if(re.search(r, url)):
+        print("Valid")
+    else:
+        print("Not Valid")
+
+url1 = "https://www.linkedin.com/"
+validateURL(url1)
+
+url2 = "http://apple"
+validateURL(url2)
+
+url3 = "iywegfuykegf"
+validateURL(url3)
+
+url4 = "https://w"
+validateURL(url4)
