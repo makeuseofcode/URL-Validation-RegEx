1ff3e235a0880c2cdbc1d74eca44fea41095564d
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2022-10-22 03:22:42 +0530

    Updated regex and addded new examples

--- urlValidator.js
+++ urlValidator.js
@@ -1,11 +1,11 @@
 function validateURL(url) {
-   if(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g.test(url)) {
+   if(/^(http(s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/g.test(url)) {
         console.log('Valid');
     } else {
         console.log('Not Valid');
     }
 }
-validateURL("https://www.linkedin.com/");
+validateURL("ljshdvcljhttps://www.linkedin.com/");
 validateURL("http://apple"); 
 validateURL("iywegfuykegf");
 validateURL("https://w"); 
--- urlValidator.py
+++ urlValidator.py
@@ -1,7 +1,7 @@
 import re
 
 def validateURL(url):
-    regex = "((http|https)://)(www.)?[-a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)"
+    regex = "^((http|https)://)[-a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)$"
     r = re.compile(regex)
 
     if(re.search(r, url)):
@@ -9,14 +9,23 @@ def validateURL(url):
     else:
         print("Not Valid")
 
-url1 = "https://www.linkedin.com/"
+url1 = "https://www.something.com/"
 validateURL(url1)
 
-url2 = "http://apple"
+url2 = "http://www.something.com/"
 validateURL(url2)
 
-url3 = "iywegfuykegf"
+url3 = "https://www.something.edu.co.in"
 validateURL(url3)
 
-url4 = "https://w"
+url4 = "https://www.url-with-querystring.com/?url=has-querystring"
 validateURL(url4)
+
+url5 = "http://url-without-www-subdomain.com/"
+validateURL(url5)
+
+url6 = "https://mail.google.com"
+validateURL(url6)
+
+url7 = "http://wrhgv675376.commmmsexxx/path"
+validateURL(url7)
